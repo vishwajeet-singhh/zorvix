1a1581baa41c816c810dc4f8219264c9cb009a6b
Vishwajeet <vishwajeet.singh@mayadataprivacy.eu>
2026-07-25 22:36:47 +0530

    clkean

--- src/main/java/com/orvix/review/NotARepositoryException.java
+++
@@ -1,9 +0,0 @@
-package com.orvix.review;
-
-/** Raised when an Orvix command is run outside a Git repository. */
-public class NotARepositoryException extends RuntimeException {
-
-    public NotARepositoryException(String message) {
-        super(message);
-    }
-}
