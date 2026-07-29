12e8d11b6ad9090a23574125eac5b36a235849b6
Recusant <110808625+recusant-byte@users.noreply.github.com>
2026-07-29 08:36:05 +0530

    Time: 3 ms (24.9%), Space: 72.2 MB (28.45%) - LeetHub

---
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int[] productExceptSelf(int[] nums) {
+
+        int n = nums.length;
+
+        int[] arr = new int[n];
+
+        arr[0] = 1;
+
+        for (int i = 1; i < n; i++) {
+            arr[i] = arr[i - 1] * nums[i - 1];
+        }
+        int postfix = 1;
+
+        for (int i = n - 1; i >= 0; i--) {
+            arr[i] = arr[i] * postfix;
+            postfix = postfix * nums[i];
+
+        }
+
+        return arr;
+
+    }
+}
