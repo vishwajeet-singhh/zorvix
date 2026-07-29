9c293af3fc1210b7e50caf883fb4a132bb9d5c70
Recusant <110808625+recusant-byte@users.noreply.github.com>
2026-07-29 08:35:54 +0530

    Time: 3 ms (58.69%), Space: 44.4 MB (55.6%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -0,0 +1,26 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+
+        s = s.toLowerCase();
+
+        int left = 0;
+        int right = s.length() - 1;
+
+        while (left < right) {
+
+            if(!Character.isLetterOrDigit(s.charAt(left))) left++;
+            else if(!Character.isLetterOrDigit(s.charAt(right))) right--;
+            else{
+                if(s.charAt(left) != s.charAt(right)) return false;
+                left++;
+                right--;
+
+            }
+
+
+        }
+
+        return true;
+
+    }
+}
