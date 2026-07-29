1d865406b8fa1bb71da36dcc0eebd3640f5acda2
Recusant <110808625+recusant-byte@users.noreply.github.com>
2026-07-29 08:37:11 +0530

    Time: 0 ms (100%), Space: 43.1 MB (96.05%) - LeetHub

---
+++ 0027-remove-element/0027-remove-element.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int removeElement(int[] nums, int val) {
+
+        int left = 0;
+        int right = 0;
+
+        while (right < nums.length) {
+            if (nums[right] != val) {
+
+                nums[left] = nums[right];
+
+                left++;
+
+            }
+
+            right++;
+
+        }
+
+        return left;
+
+    }
+}
