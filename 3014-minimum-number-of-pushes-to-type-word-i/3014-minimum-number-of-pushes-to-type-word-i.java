c1cd3534220f5781de284313a4ef759796e9ad07
Recusant <110808625+recusant-byte@users.noreply.github.com>
2026-07-30 10:25:30 +0530

    Time: 0 ms (100%), Space: 43.2 MB (66.51%) - LeetHub

---
+++ 3014-minimum-number-of-pushes-to-type-word-i/3014-minimum-number-of-pushes-to-type-word-i.java
@@ -0,0 +1,14 @@
+class Solution {
+
+    public int minimumPushes(String word) {
+
+        int ans = 0;
+
+        for (int i = 0; i < word.length(); i++) {
+            ans += (i / 8) + 1;
+        }
+
+        return ans;
+
+    }
+}
