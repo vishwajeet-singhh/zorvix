3b86a5d4c7087cf27bc3d74301322febf2ae3996
Recusant <110808625+recusant-byte@users.noreply.github.com>
2026-07-31 17:51:23 +0530

    Time: 0 ms (100%), Space: 44.2 MB (75.91%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.java
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.java
@@ -11,10 +11,11 @@
 class Solution {
     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
 
-        ListNode dummy = new ListNode(-1);
-        ListNode tail = dummy;
+        ListNode merged = new ListNode(-1);
+        ListNode tail = merged;
 
         while (list1 != null && list2 != null) {
+
             if (list1.val <= list2.val) {
                 tail.next = list1;
                 list1 = list1.next;
@@ -22,16 +23,15 @@ class Solution {
                 tail.next = list2;
                 list2 = list2.next;
             }
+
             tail = tail.next;
+
         }
 
-        if (list1 != null) {
-            tail.next = list1;
-        } else {
-            tail.next = list2;
-        }
+        if(list1 != null) tail.next = list1;
+        else tail.next = list2;
 
-        return dummy.next;
-        
+        return merged.next;
+
     }
 }
