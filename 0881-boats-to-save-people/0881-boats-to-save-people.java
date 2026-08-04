d24cbc550c14ead2bfbc2dcc57978395d8f83b5d
Recusant <110808625+recusant-byte@users.noreply.github.com>
2026-08-04 16:38:58 +0530

    Time: 20 ms (82.99%), Space: 56.6 MB (20.94%) - LeetHub

---
+++ 0881-boats-to-save-people/0881-boats-to-save-people.java
@@ -0,0 +1,37 @@
+class Solution {
+    public int numRescueBoats(int[] people, int limit) {
+
+        //[3,2,2,1] limit = 3
+        int left = 0, right = people.length - 1, result = 0;
+
+        Arrays.sort(people);
+
+        //[1,2,2,3] limit = 3
+        // left = 0, right = 3
+
+        while (left <= right) {
+
+            // I- 1:
+            // left = 0 (1), right = 3 (3)
+            // 1 + 3 = 4 > 3 -> can't pair
+
+            // Iteration 2:
+            // left = 0 (1), right = 2 (2)
+            // 1 + 2 = 3 <= 3 -> pair them
+            // left = 1
+
+            // Iteration 3:
+            // left = 1 (2), right = 1 (2)
+            // 2 + 2 = 4 > 3 -> can't pair
+
+            if (people[left] + people[right] <= limit) {
+                left++;   // l = 1 after iteration 2
+            }
+
+            right--;      // r = 2, 1, 0
+            result++;     // boats = 1, 2, 3
+        }
+
+        return result;
+    }
+}
