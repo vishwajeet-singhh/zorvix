874525587786d6f6d8241b73dc188f2da83eb846
Vishwajeet <vishwajeet.singh@mayadataprivacy.eu>
2026-08-14 10:05:21 +0530

    14 aug

---
+++ src/main/java/com/orvix/codeforce/CF_228A.java
@@ -0,0 +1,23 @@
+package com.orvix.codeforce;
+
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
+
+public class CF_228A {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+
+        Set<Integer> set = new HashSet<>();
+
+        int n = 4;
+        while (n-- > 0) {
+            set.add(scan.nextInt());
+        }
+
+        System.out.println(4 - set.size());
+
+    }
+
+}
